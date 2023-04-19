14b10e5bf4bfddb43c09a6e38bca840fe3fb3f34
SeanMcLL <seanmcl135@gmail.com>
2023-04-18 20:33:29 -0400

    Game Manager + Reset
    Created Game Manager singleton and added functionality such that you can reset the match when the player count goes to zero.

--- Nittany Arena/Assets/Code/Scripts/State Machine/StateMachine.cs
+++ Nittany Arena/Assets/Code/Scripts/State Machine/StateMachine.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
+<<<<<<< Updated upstream
 
 public class StateMachine : MonoBehaviour
 {
+=======
+using Prime31;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class StateMachine : MonoBehaviour
+{
+    GameManager gameManager;
+>>>>>>> Stashed changes
     private class ChangeStateCommand
+    
     {
+        
         private StateMachine m_StateMachine;
         private BaseState m_NewState;
 
+<<<<<<< Updated upstream
         public ChangeStateCommand(StateMachine stateMachine, BaseState newState)
+=======
+        
+
+        public ChangeStateCommand(StateMachine stateMachine, PlayerStateFactory stateFactory, BaseState newState)
+>>>>>>> Stashed changes
         {
             m_StateMachine = stateMachine;
             m_NewState = newState;
@@ -32,6 +50,7 @@ public class StateMachine : MonoBehaviour
     private ChangeStateCommand m_ChangeState = null;
     private PlayerStateFactory m_PlayerStateFactory = null;
 
+    [SerializeField] private InputData m_inputData;
     [SerializeField] private BaseState m_StartState;
 
     private void Awake()
@@ -42,6 +61,16 @@ public class StateMachine : MonoBehaviour
 
     private void Start()
     {
+<<<<<<< Updated upstream
+=======
+        
+        //TODO: get player count reference proper
+        //m_PlayerCount = inputMatcher.m_PlayerCount;
+        gameManager = GameManager.Instance;
+
+        m_Controller.onTriggerExitEvent += OnColliderTriggerExit;
+
+>>>>>>> Stashed changes
         m_CurrentState = m_PlayerStateFactory.GroundedState;
         m_CurrentState.StateFactory = m_PlayerStateFactory;
 
@@ -50,6 +79,32 @@ public class StateMachine : MonoBehaviour
             m_CurrentState.StateManager = this;
             m_CurrentState.OnEnter();
         }
+<<<<<<< Updated upstream
+=======
+    }
+
+    private void OnColliderTriggerExit(Collider2D obj)
+    {
+        if (obj.gameObject.layer == LayerMask.NameToLayer("BlastZone"))
+        {
+            m_Player.stocks--;
+
+            if (m_Player.stocks == 0)
+            {
+                //Destroy(gameObject);
+                //GameData.DisablePlayer();
+                gameManager.PlayerDefeated();
+
+                return;
+            }
+
+            m_Player.damagePercent = 0;
+            transform.position = m_SpawnPoint.position;
+            m_Controller.move(Vector2.zero);
+            ChangeState(m_PlayerStateFactory.FallState);
+            m_ChangeState.Execute();
+        }
+>>>>>>> Stashed changes
     }
 
     private void FixedUpdate()
@@ -58,10 +113,42 @@ public class StateMachine : MonoBehaviour
 
         m_ChangeState?.Execute();
         m_ChangeState = null;
+<<<<<<< Updated upstream
+=======
+        //Debug.Log("button pressed status: " + m_inputData.CurrentFrame.ButtonSouth);
+       
+    
+        if (gameManager.gameOver && m_inputData.CurrentFrame.ButtonSouth)
+        {
+            Debug.Log("button was pressed");
+            gameManager.ReloadScene();
+        }
+    
+>>>>>>> Stashed changes
     }
 
     public void ChangeState(BaseState state)
     {
         m_ChangeState = new ChangeStateCommand(this, state);
     }
+<<<<<<< Updated upstream
+=======
+
+    //TODO: Add arguments
+    public void TakeDamage(Vector2 baseDirection, float baseKnockback, float knockbackGrowth)
+    {
+        //Calculate knockback
+        m_PlayerStateFactory.KnockbackState.SetKnockback(baseDirection.normalized * (baseKnockback + knockbackGrowth * m_Player.damagePercent));
+
+        //Add damage
+        m_Player.damagePercent += 5.0f;
+
+        //Change to knockback state
+        ChangeState(m_PlayerStateFactory.KnockbackState);
+    }
+
+    
+
+    
+>>>>>>> Stashed changes
 }
---
+++ Nittany Arena/Assets/Code/Scripts/UI/StartMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private InputData m_inputData;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_inputData.CurrentFrame.ButtonSouth)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single); // Load the persistent scene asynchronously
+            SceneManager.LoadScene("HitboxTestingZone", LoadSceneMode.Additive); // Load the new scene additively
+        }
+    }
+}
